e6db7f5b0d72a178b0f9ea624006ce855ec5033a
kuroyasu <xjr1300.04@gmail.com>
2025-06-21 16:56:39 +0900

    Introduce the react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { Routes, Route } from 'react-router';
 import { Button } from './components/ui/button';
 
 const App = () => {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <Routes>
+      <Route
+        index
+        element={
+          <div className="flex min-h-svh flex-col items-center justify-center">
+            <Button>Click me</Button>
+          </div>
+        }
+      />
+      <Route path="auth">
+        <Route path="sign-up" element={<div>サインアップページ</div>} />
+        <Route path="login" element={<div>ログインページ</div>} />
+        <Route path="logout" element={<div>ログアウトページ</div>} />
+      </Route>
+    </Routes>
   );
 };
 
